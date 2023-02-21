196e28d3843c36225b79de5363a4e004a3b419e8
Miroslav Bauer <mirekys@users.noreply.github.com>
2023-02-21 18:08:16 +0100

    working oc-reva k8s deploy

--- k8s/sciencemesh-site/templates/_helpers.tpl
+++
@@ -1,3 +0,0 @@
-{{- define "owncloud.fullname" -}}
-server
-{{- end -}}
--- k8s/tests/nc2oc/deploy.sh
+++ k8s/tests/nc2oc/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-helm install -n nc-site --create-namespace nextcloud -f nc.yaml ../../sciencemesh-site
-helm install -n oc-site --create-namespace owncloud -f oc.yaml ../../sciencemesh-site
+# helm install -n nc-site --dry-run --debug --create-namespace nextcloud -f nc.yaml ../../sciencemesh-site
+helm install -n oc-site --create-namespace owncloud -f oc.yaml ../../owncloud

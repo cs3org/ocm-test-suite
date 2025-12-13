211d2154531fa2b33259c11c50ce0e3145927687
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2025-12-13 09:15:57 +0000

    fix: cleaning with wayf suffixes

--- scripts/utils/test_modes/ci.sh
+++ scripts/utils/test_modes/ci.sh
@@ -56,8 +56,21 @@ run_ci() {
 
         # Build argument list for clean.sh.
         # WAYF scenarios use dedicated *-wayf containers. IdP remains a singleton idp.docker.
+        # Login v33 (nextcloud) and v2 (cernbox) also use *-wayf containers.
         local clean_args=("no" "cypress" "meshdir" "firefox" "vnc")
+        local uses_wayf_containers="false"
         if [[ "${TEST_SCENARIO}" == "wayf" ]]; then
+            uses_wayf_containers="true"
+        elif [[ "${TEST_SCENARIO}" == "login" ]]; then
+            # Login v33 (nextcloud) and v2 (cernbox) use wayf containers
+            if [[ "${EFSS_PLATFORM_1}" == "nextcloud" && "${EFSS_PLATFORM_1_VERSION}" == "v33" ]]; then
+                uses_wayf_containers="true"
+            elif [[ "${EFSS_PLATFORM_1}" == "cernbox" && "${EFSS_PLATFORM_1_VERSION}" == "v2" ]]; then
+                uses_wayf_containers="true"
+            fi
+        fi
+        
+        if [[ "${uses_wayf_containers}" == "true" ]]; then
             clean_args+=("idp" "${EFSS_PLATFORM_1}-wayf")
             if [[ "${TEST_SCENARIO}" != "login" ]]; then
                 clean_args+=("${EFSS_PLATFORM_2}-wayf")
